8b2faee51d48837ed18c166687868a242fdcd067
Roshanleo95 <roshanleo95@gmail.com>
2022-12-27 18:15:51 +0545

    DB Configuration, mysqlclient installed & model migration created-migrated

--- project_inventory/app_inventory/models.py
+++ project_inventory/app_inventory/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class AppUser(models.Model):
     full_name = models.CharField(max_length=200)
-    email = models.Emailfield(unique=True)
+    email = models.EmailField(unique=True)
     contact = models.CharField(max_length=100)
     password = models.CharField(max_length=100)
 
